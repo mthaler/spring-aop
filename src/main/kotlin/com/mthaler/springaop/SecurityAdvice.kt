421a180056dbbce1e9ed6e997befdf9e375b56d4
Michael Thaler <michael.thaler@dlr.de>
2021-06-19 12:09:23 +0200

    Updated SecurityAdvice.

--- src/main/kotlin/com/mthaler/springaop/SecurityAdvice.kt
+++ src/main/kotlin/com/mthaler/springaop/SecurityAdvice.kt
@@ -9,12 +9,12 @@ class SecurityAdvice(val securityManager: SecurityManager): MethodBeforeAdvice {
         val user = securityManager.getLoggedOnUser()
 
         if (user == null) {
-            System.out.println("No user authenticated");
+            println("No user authenticated");
             throw SecurityException("You must login before attempting to invoke the method: ${method.getName()}")
         } else if ("John" == user.userName) {
-            System.out.println("Logged in user is John - OKAY!")
+            println("Logged in user is John - OKAY!")
         } else {
-            System.out.println("Logged in user is ${user.userName} NOT GOOD :(")
+            println("Logged in user is ${user.userName} NOT GOOD :(")
             throw SecurityException("User ${user.userName} is not allowed access to method ${method.getName()}")
         }
     }
