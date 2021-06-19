72b8077f1cca37ec9cf621843356d97211e74b66
Michael Thaler <michael.thaler@dlr.de>
2021-06-19 12:01:04 +0200

    Added SecureBean.

---
+++ src/main/kotlin/com/mthaler/springaop/SecureBean.kt
@@ -0,0 +1,8 @@
+package com.mthaler.springaop
+
+class SecureBean {
+
+    fun writeSecureMessage() {
+        println("Every time I learn something new, it pushes some old stuff out of my brain")
+    }
+}
